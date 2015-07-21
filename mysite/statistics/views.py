bf7dd164e6544bc158bd49626846e1f3fd500d73
Mohit Tiwari <mo@Mohits-MacBook-Pro.local>
2015-07-20 22:35:06 -0700

    Adding auto refresh

--- mysite/statistics/views.py
+++ mysite/statistics/views.py
@@ -9,12 +9,24 @@ import json
 # Create your views here.
 def index(request):
 	messages_manager = Messages.objects
-	print messages_manager
-	distinct_cities = Set([x.city for x in messages_manager.all()])
-	distinct_users = Set([x.username for x in messages_manager.all()])
+	try:
+		distinct_cities = Set([x.city for x in messages_manager.all()])
+		distinct_users = Set([x.username for x in messages_manager.all()])
+		'''print distinct_users[4]'''
+
+		response = HttpResponse(json.dumps({
+			'result':'success',
+			'cities': len(distinct_cities),
+			'users': len(distinct_users)
+			}))
+		response['refresh'] = 60
 		
-	return HttpResponse(json.dumps({
-		'result':'success',
-		'cities': len(distinct_cities),
-		'users': len(distinct_users)
-		}))
+		return response
+
+
+	except Exception,e:
+		return HttpResponse(json.dumps({
+			'result':'error',
+			'error': e
+			}))
+
